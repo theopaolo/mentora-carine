265313a29ddfd6c838865713ec22370146e0a1f3
Carine <carinerandrianarijaona@gmail.com>
2025-08-05 17:41:03 +0200

    update: kata 2

--- mini-katas/katas_product_engineer/kata_2_generateur_idees_aleatoires/script.js
+++ mini-katas/katas_product_engineer/kata_2_generateur_idees_aleatoires/script.js
@@ -1,32 +1,40 @@
+// lancer le serveur python: python3 -m http.server 8000
 const findIdeaButton = document.querySelector("button");
 const randomIdea = document.querySelector("p");
+const corsProxy = "https://corsproxy.io/?";
+const apiURL = "https://bored-api.appbrewery.com";
+let dataActivity = "";
 
-function recupererIdee() {
-    fetch('https://www.boredapi.com/api/activity')
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data.activity)
-        })
-        .catch((error) => {
-            console.log("Erreur :", error);
-        });
+
+
+// function recupererIdee() {
+//     fetch(`${corsProxy}${apiURL}/random`) 
+//         .then((response) => response.json())
+//         .then((data) => {
+//             console.log(data.activity)
+//             randomIdea.textContent = data.activity;
+//         })
+//         .catch((error) => {
+//             console.log("Erreur :", error);
+//         });
+// }
+
+// console.log(dataActivity)
+
+async function recupererIdee() {
+    try {
+        const response = await fetch(`${corsProxy}${apiURL}/random`);
+        const data = await response.json();
+        return data.activity;
+    } catch (error) {
+        console.log("Erreur :", error);
+    }
 }
 
-findIdeaButton.addEventListener("click", () => {
-    const activity = recupererIdee();
+findIdeaButton.addEventListener("click", async() => {
+    const activity = await recupererIdee();
     randomIdea.textContent = activity;
 })
 
-// async function recupererIdee() {
-//         try {
-//             const response = await fetch('https://www.boredapi.com/api/activity');
-//             const data = await response.json();
-//             document.getElementById("idee").textContent = data.activity;
-//         } catch (error) {
-//             console.log("Erreur :", error);
-//         }
-//     }
-
-// recupererIdee();
 
 
